8d401cfc3827236cebe18fd85b63463a2fd597a6
migueltcosta03 <migueltcosta2003@gmail.com>
2023-12-30 01:22:21 +0000

    - Adicionada atualização automática do ano.
    - Adicionada atualização automática do ano no footer.

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ import bodyParser from "body-parser";
 const app = express();
 const port = process.env.PORT || 1812;
 
+// Page Configuration
+const currentYear = new Date().getFullYear();
+
 // Middlewares
 app.use(express.static("public"));
 
 // Routes
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+        currentYear: currentYear,
+    });
 })
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
